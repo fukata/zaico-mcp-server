fe547efe811986ffa8387aadee803bd6895fc8bb
fukata <tatsuya.fukata@gmail.com>
2025-04-05 19:11:01 +0700

    wip

--- cmd/zaico-mcp-server/main.go
+++ cmd/zaico-mcp-server/main.go
@@ -1,5 +1,36 @@
 package main
+import (
+	"fmt"
+	"os"
 
-func main() {
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "zaico-mcp-server",
+	Short: "Zaico MCP Server",
+	Long:  "Zaico MCP Server is a server application for MCP protocol",
+	Run: func(cmd *cobra.Command, args []string) {
+		if viper.GetBool("verbose") {
+			logrus.SetLevel(logrus.DebugLevel)
+		} else {
+			logrus.SetLevel(logrus.InfoLevel)
+		}
+		logrus.Info("Starting Zaico MCP Server...")
+	},
+	Version: "1.0.0",
 }
 
+func init() {
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Enable verbose logging")
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
